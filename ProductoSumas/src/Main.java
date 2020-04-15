7dc5d4e71f54c463fec92d899a70a8cf2aee083f
gonzo-hub <59870653+gonzo-hub@users.noreply.github.com>
2020-04-15 17:32:57 +0200

    Producto mediante sumas

---
+++ ProductoSumas/src/Main.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner (System.in);
+		System.out.println("Introduzca el primer número: ");
+		int a = teclado.nextInt();
+		System.out.println("Introduzca el segundo número: ");
+		int b = teclado.nextInt();
+		
+		System.out.println("El resultado de multiplicar " + a + " por " + b + " es: " + multiplicar(a,b));
+	}
+
+
+public static int multiplicar(int a, int b) {
+	
+	if (a==0) {
+		return 0;
+	}
+	
+	if (b==0){
+		return 0;}
+	
+	else 
+		return a + multiplicar(a,b-1);
+}
+}
